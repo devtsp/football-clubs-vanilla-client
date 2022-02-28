b9a73c15a50d76eb22ca2913d1ec26da593574f9
Tomas <87679143+devtsp@users.noreply.github.com>
2022-02-27 21:44:40 -0300

    list details and create on point.
    Proceed to work onhandling  form submissions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-const getClubs = async () => {
-	const response = await fetch('http://localhost:8080/');
+const fetchIndex = async () => {
+	const response = await fetch('http://localhost:8080/index');
+	const data = response.json();
+	return data;
+};
+
+const fetchClub = async id => {
+	const response = await fetch(`http://localhost:8080/${id}`);
 	const data = response.json();
 	return data;
 };
@@ -28,7 +34,7 @@ const handlePanelsVisibility = id => {
 	});
 };
 
-const handleDelete = async id => {
+const fetchDelete = async id => {
 	const options = {
 		method: 'DELETE',
 		headers: { 'Content-Type': 'application/json' },
@@ -38,7 +44,29 @@ const handleDelete = async id => {
 	return data;
 };
 
-const fillDetails = club => {
+const handleDelete = async id => {
+	fetchDelete(id);
+	document.querySelector(`#${id}`).remove();
+};
+
+const fillEditForm = club => {
+	document.querySelector('#edit-form').dataset.clubId = club.id;
+	handleColorInput(club);
+	const $inputs = document.querySelectorAll('.edit-input');
+	$inputs.forEach($input => {
+		$input.value = club[`${$input.name}`];
+	});
+	return [...$inputs];
+};
+
+const fillDetails = async id => {
+	const club = await fetchClub(id);
+	document.querySelector('#club-edit-link').dataset.clubId = club.id;
+	document.querySelector('#club-edit-link').onclick = e => {
+		document.querySelector('#club-edit-link').click();
+		handlePanelsVisibility('club-edit');
+		fillEditForm(club);
+	};
 	document.querySelector(
 		'#crest-main img'
 	).src = `http://localhost:8080/uploads/img/${club.crest}`;
@@ -51,60 +79,97 @@ const fillDetails = club => {
 	).style.backgroundImage = `linear-gradient(to right, transparent, ${club.colors[0]} 20%, ${club.colors[0]} 50%, ${club.colors[1]} 50%, ${club.colors[1]} 80%, transparent)`;
 };
 
-const fillEditForm = club => {
-	console.log(club.colors[0]);
-	console.log(club.colors[1]);
-	document.querySelectorAll('.edit-input').forEach($input => {
-		$input.value = club[`${$input.name}`];
+const handleColorInput = club => {
+	const $color1Prev = document.querySelector('#color-1');
+	const $color2Prev = document.querySelector('#color-2');
+	const $color1 = $color1Prev.cloneNode(true);
+	const $color2 = $color2Prev.cloneNode(true);
+	document.querySelector('#color1-field-container').children[0].remove();
+	document.querySelector('#color2-field-container').children[0].remove();
+	document.querySelector('#color1-field-container').appendChild($color1);
+	document.querySelector('#color2-field-container').appendChild($color2);
+	$color1.value = club.colors[0];
+	$color2.value = club.colors[1];
+	document
+		.querySelector('#color-1')
+		.classList.add('form-control', 'edit-color');
+	document
+		.querySelector('#color-2')
+		.classList.add('form-control', 'edit-color');
+	return [$color1, $color2];
+};
+
+const resetNavLinksStyle = () => {
+	document.querySelectorAll('nav a').forEach($tab => {
+		$tab.classList.remove('text-decoration-none', 'fw-bold', 'text-black');
+		$tab.classList.add('text-black-50');
 	});
-	console.log(document.querySelector('#color-1'));
-	console.log(document.querySelector('#color-2'));
-	document.querySelector('#color-1').value = club.colors[0];
-	document.querySelector('#color-2').value = club.colors[1];
-	document.querySelector('#color-1').style.backgroundColor = club.colors[0];
-	document.querySelector('#color-2').style.backgroundColor = club.colors[1];
-	console.log(document.querySelector('#color-1'));
-	console.log(document.querySelector('#color-2'));
+};
+
+const setIndexEvents = $clubItem => {
+	$clubItem.querySelector('.delete-club').onclick = e =>
+		handleDelete(e.target.dataset.clubId);
+	$clubItem.querySelector('.club-detail-link').onclick = e => {
+		resetNavLinksStyle();
+		fillDetails(e.target.dataset.clubId);
+		handlePanelsVisibility('club-details');
+	};
 };
 
 const renderClubItem = club => {
 	const $clubItem = document
 		.querySelector('#club-item-template')
 		.cloneNode(true);
-	$clubItem.classList.remove('visually-hidden');
 	$clubItem.id = club.id;
+	$clubItem.querySelector('.club-delete-link').dataset.clubId = club.id;
+	$clubItem.querySelector('.club-detail-link').dataset.clubId = club.id;
 	$clubItem.querySelector('.club-detail-link').innerText = club.name;
 	$clubItem.querySelector(
 		'img'
 	).src = `http://localhost:8080/uploads/img/${club.crest}`;
-	$clubItem.querySelector('.delete-club').onclick = () => handleDelete(club.id);
-	$clubItem.querySelector('.club-detail-link').onclick = () => {
-		document.querySelectorAll('nav a').forEach($tab => {
-			$tab.classList.remove('text-decoration-none', 'fw-bold', 'text-black');
-			$tab.classList.add('text-black-50');
-		});
-		// handlePanelsVisibility('club-details');
-		fillDetails(club);
-		fillEditForm(club);
-	};
-	// $clubItem.querySelector('#club-edit-link').onclick = e => {
-	// 	fillEditForm(club);
-	// };
+	$clubItem.classList.remove('visually-hidden');
 	document.querySelector('#all-clubs').appendChild($clubItem);
+	setIndexEvents($clubItem);
+	return $clubItem;
+};
+
+const displayIndexOfClubs = async () => {
+	const index = await fetchIndex();
+	if (!index.length) {
+		document.querySelector('#no-clubs').classList.remove('visually-hidden');
+	} else {
+		document.querySelector('#no-clubs').classList.add('visually-hidden');
+		[...document.querySelector('#all-clubs').children].forEach(club => {
+			club.remove();
+		});
+		index.forEach(club => renderClubItem(club));
+	}
+	return document.querySelector('#all-clubs');
 };
 
 document.querySelector('nav').onclick = e => {
 	if (e.target.tagName == 'A') {
 		handleNavActive(e);
 		handlePanelsVisibility(e.target.id);
+		return e.target.id;
 	}
 };
 
-getClubs().then(clubs => {
-	if (!clubs.length) {
-		document.querySelector('#no-clubs').classList.remove('visually-hidden');
-	} else {
-		document.querySelector('#no-clubs').classList.add('visually-hidden');
-		clubs.forEach(club => renderClubItem(club));
-	}
-});
+document.querySelector('#club-list-tab').onclick = e => {
+	displayIndexOfClubs();
+};
+
+document.querySelector('#edit-form').onsubmit = async e => {
+	e.preventDefault();
+	const options = {
+		method: 'POST',
+		headers: { 'Content-Type': 'form-data' },
+	};
+	const response = await fetch(
+		`http://localhost:8080/edit/${e.target.dataset.clubId}`,
+		options
+	);
+	const data = await response.json();
+	console.log(data);
+	// document.querySelector('#club-list-tab').click();
+};
